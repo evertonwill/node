05ba666b555b8ca85431769dbb537c608b43383b
evertonwill <123010053+evertonwill@users.noreply.github.com>
2023-06-18 18:14:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname, 'index.html');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
